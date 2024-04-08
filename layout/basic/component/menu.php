a9784b08ac8490d022d60fb2b647dbcc31c5a06f
my3rdlife <my3rdstory@gmail.com>
2024-04-08 10:06:10 +0900

    상단 커스텀 링크 여분필드 처리

--- layout/basic/component/menu.php
+++ layout/basic/component/menu.php
@@ -5,8 +5,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 
 <nav class="site-nav d-none d-lg-block" role="navigation">
-    <a href="http://info.nec.go.kr/main/showDocument.xhtml?electionId=0020240410&topMenuId=BI&secondMenuId=BIAP01">사전투표현황</a>
-    <a href="/notice" class="nav-link">공지사항</a>
-    <a href="/bbs/group.php?gr_id=group" class="nav-link">소모임</a>
-    <a href="/bbs/group.php?gr_id=community" class="nav-link">커뮤니티</a>
+  <a href="<?php echo $config['cf_8'];?>" target="_blank">투표소찾기</a>
+  | <a href="/notice" class="nav-link">공지사항</a> | <a href="/bbs/group.php?gr_id=group" class="nav-link">소모임</a> | <a
+    href="/bbs/group.php?gr_id=community" class="nav-link">커뮤니티</a>
 </nav>
