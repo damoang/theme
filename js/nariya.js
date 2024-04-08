8bc31e450ee073ecf5dbe28b84a0c9a014d37529
yj.yoon <yj.yoon@altimedia.com>
2024-04-08 15:22:07 +0900

    추천 후  Alert 삭제

--- js/nariya.js
+++ js/nariya.js
@@ -263,12 +263,12 @@ function na_good(bo_table, wr_id, act, id, opt) {
 			na_alert(data.error, function(){}, 2500);
 			return false;
 		} else if(data.success) {
-			na_alert(data.success, function(){
+			//na_alert(data.success, function(){	추천후 Alert 제거
 				$("#"+id).text(number_format(String(data.count)));
 				if($("."+id+'_cnt').length) {
 					$("."+id+'_cnt').text(number_format(String(data.count)));
 				}				
-			}, 2500);
+			//}, 2500);
 		}
 	}, "json");
 }
