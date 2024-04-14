6da2d579543d2ac1f493970e21f068d24a178711
sundo-kim <sundo.kim@bespinglobal.com>
2024-04-14 20:33:53 +0900

    Arch 님 UI 반영

--- layout/basic/head.sub.php
+++ layout/basic/head.sub.php
@@ -149,7 +149,7 @@ $one_cols = array(
                         }
                         //미리보기창 끄기
                         if (ui_obj.img_preview != null && ui_obj.img_preview) {
-                            ui_custom_style += "div.popover.bs-popover-auto.fade.show_ {display: none !important;}\n";
+                            ui_custom_style += "div.popover.bs-popover-auto.fade.show {display: none !important;}\n";
                         }
 
                         //root style 글씨체 및 크기
