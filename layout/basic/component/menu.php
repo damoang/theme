ca34cbfd810d36206a05bf0b355d0587be425bd3
sundo-kim <sundo.kim@bespinglobal.com>
2024-04-14 11:19:24 +0900

    git 주소 추가

--- layout/basic/component/menu.php
+++ layout/basic/component/menu.php
@@ -7,7 +7,8 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 <nav class="site-nav d-none d-lg-block" role="navigation">
   <?php if (!empty($config['cf_8'])): ?>
   <a href="<?php echo $config['cf_8'];?>" target="_blank"><?php echo $config['cf_8_subj']?></a> |
-  <?php endif; ?>  
+  <?php endif; ?>
   <a href="/notice" class="nav-link">공지사항</a> | <a href="/bbs/group.php?gr_id=group" class="nav-link">소모임</a> | <a
-    href="/bbs/group.php?gr_id=community" class="nav-link">커뮤니티</a>
+    href="/bbs/group.php?gr_id=community" class="nav-link">커뮤니티</a> |
+    <a href="https://github.com/damoang/theme/pulls?q=" target="_blank">내가 만드는 커뮤니티!</a>
 </nav>
