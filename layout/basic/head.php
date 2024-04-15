b398226b85f37e2d29a0008f378d4bf1470ab6a0
sundo-kim <sundo.kim@bespinglobal.com>
2024-04-16 02:02:22 +0900

    메인에 푸시 하자 ~~ 어차치 깃은 꼬였음..ㅠ

--- layout/basic/head.php
+++ layout/basic/head.php
@@ -128,8 +128,3 @@ if(!IS_INDEX) {
 
 
 <?php } // 메인이 아닐 경우 ?>
-<?php if (!empty($bo_table)): ?>
-    <a href="/<?php echo $bo_table ?>?sca=&sfl=mb_id,1&stx=<?php echo $member['mb_id'] ?>">내글 보기</a> |
-<a href="/<?php echo $bo_table ?>?bo_table=notice&sca=&sfl=mb_id%2C0&sop=and&stx=<?php echo $member['mb_id'] ?>"> 내 댓글 보기
-
-<?php endif; ?>
