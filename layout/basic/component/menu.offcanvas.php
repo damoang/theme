18eee7a83a3ad6d9e796e9c19752a76da47ae779
kkigomi <kkigomi@duck.com>
2024-04-25 23:54:30 +0900

    fix: 잘못 제거된 알림 링크 복구
    충돌 수정 시 잘못 제거된 알림보기 페이지 링크 복구

--- layout/basic/component/menu.offcanvas.php
+++ layout/basic/component/menu.offcanvas.php
@@ -32,8 +32,7 @@ if (!empty($config['cf_9'])) {
 }
 ?>
         <div class="nav-item nav-link">
-          <a href="/bbs/new.php?bo_table=&sca=&gr_id=&view=w&mb_id=<?php echo $member['mb_id'] ?>">내 글</a> |
-          <a href="/bbs/new.php?bo_table=&sca=&gr_id=&view=c&mb_id=<?php echo $member['mb_id'] ?>">내 댓글</a> | <a
+          <a href="/bbs/new.php?bo_table=&sca=&gr_id=&view=w&mb_id=<?php echo $member['mb_id'] ?>">내 글</a>&nbsp;|&nbsp;<a href="/bbs/new.php?bo_table=&sca=&gr_id=&view=c&mb_id=<?php echo $member['mb_id'] ?>">내 댓글</a>&nbsp;|&nbsp;<a
             href="/bbs/noti.php"><i class="bi bi-bell"></i>알림</a>
         </div>
         <!-- 사이드 메뉴 -->
