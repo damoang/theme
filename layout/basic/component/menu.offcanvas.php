860fc032f285cdfdc13953ec7b7b2444b7a4a774
kkigomi <kkigomi@duck.com>
2024-05-02 13:28:24 +0900

    fix: 사이드바 메뉴에서 변수명 오타로 인한 문제 고침

--- layout/basic/component/menu.offcanvas.php
+++ layout/basic/component/menu.offcanvas.php
@@ -59,8 +59,9 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
                             $menuItem['url'] = $menuItem['url'] ?? '#';
                             $menuItem['icon'] = $menuItem['icon'] ?? 'bi-clipboard';
                             $hasSub = !empty($menuItem['items']) && is_array($menuItem['items']);
+                            $menuItem['page_id'] = $menuItem['page_id'] ?? '';
 
-                            if (!$menuitme['page_id']) {
+                            if (!$menuItem['page_id']) {
                                 if (preg_match('/\/([a-zA-Z0-9]+)$/i', $menuItem['url'], $matches)) {
                                     $menuItem['page_id'] = G5_BBS_DIR . '-board-' . $matches[1];
                                 }
