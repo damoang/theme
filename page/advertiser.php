621a806e08c4cf28bd69cd88bc2729444400b645
김선도 <98006861+damaong@users.noreply.github.com>
2024-05-27 06:54:06 +0900

    Update advertiser.php

--- page/advertiser.php
+++ page/advertiser.php
@@ -21,7 +21,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     <a href="https://www.griun.co.kr/?utm_source=damoang&utm_medium=banner"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_KaHgzveh_4dbfb09b1902d5fb477d8e8e988dc9d0ce639d57.webp">
     <a href="https://smartstore.naver.com/skcwindowfilm/products/438041022"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_uLbzS9tr_ab700a98eaf5bff85b60ce14379e5960bf463b09.webp"></a><br/><br/>
     <a href="https://bit.ly/3ymGlEj "><img src="https://damoang.net/data/file/banners/f28b5896bb5b70ec2e61f0a18713be5a_BehOx5U6_87f0076c5bba3fc00b8d6db96d24ea8bf836ae65.jpg"></a><br/><br/>
-    6번 째 사이드바 광고주님 자리 <br/><br/>
+    6번 째 사이드바 광고주님 자리 {emo:damoang-emo-029.gif:50} <br/><br/>
     7번 째 사이드바 광고주님 자리 <br/><br/>
     8번 째 사이드바 광고주님 자리 <br/><br/>
     9번 째 사이드바 광고주님 자리 <br/><br/>
