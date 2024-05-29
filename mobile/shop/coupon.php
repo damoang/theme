0288e5a2ad36d3dfe21ba761425b33199662b958
kkigomi <kkigomi@duck.com>
2024-05-30 01:30:57 +0900

    feat: 영카트 관련 스킨 및 파일 제거

--- mobile/shop/coupon.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
-
-include_once(G5_THEME_SHOP_PATH.'/coupon.php');
-return;
--- mobile/skin/shop/basic/couponzone.10.skin.php
+++
@@ -1,3 +0,0 @@
-<?php
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
-include_once(G5_SHOP_SKIN_PATH.'/couponzone.10.skin.php');
