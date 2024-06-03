60dd8e332134c0bf1b5f8a96b9e0566e90b53ed7
sdk <sdk@sdk.xyz>
2024-06-04 02:59:17 +0900

    광고주 게시판

--- page/advertiser.php
+++ page/advertiser.php
@@ -4,26 +4,31 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 <div>
     <h3>긴 배너</h3>
-    <a href="https://smartstore.naver.com/oilpangone/products/9013411649"><img src="https://damoang.net/data/editor/2405/comment_237388223_aUnJ5cSX_fc8019b56b15177a0aadee58cd98def4a76a91e8.jpg" ></a><br/><br/>
-    <a href="https://smartstore.naver.com/souplab033/products/7792638781" ><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_fy36sYPM_4a140bd3f05852b90c5cfe639af9bdc7740f1443.webp"</a><br/><br/>
-    <a href="https://smartstore.naver.com/dingreen"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_eIEiASdk_ae77791f0b971a127e0c4ea6f9bbba4692a8de02.gif"></a><br/><br/>
-    <a href="https://mj123123.cafe24.com/intking"><img src="https://damoang.net/data/editor/2405/comment_3716871170_F28Rqvax_b69648f787231953208fb7d3573a7d34e73e2cee.gif"></a><br/><br/>
-    <a href="https://smartstore.naver.com/bonnemere/products/6780017941"><img src="https://damoang.net/data/editor/2405/comment_3716871170_RBcEJt9s_ef530bc5cfbe76483eebb37ccc02587bee1897c5.gif"></a><br/><br/>
-    <a href="https://buddyback.co.kr/category/%EC%8A%A4%ED%8A%B8%EB%A0%88%EC%B9%AD%EA%B8%B0%EC%96%B4/24/"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_JNwKLlnF_3a8373b26cc78a8aba81d41893e6fa1edcd7960a.webp"></a><br/><br/>
-    <a href="https://1chopick.com/product/list.html?cate_no=61"><img src="https://damoang.net/data/editor/2405/comment_2949741484_hAFuV1z0_8119986b536b8afdd8153ad1a297081d8c22b0e8.webp"></a><br/><br/>
-    <a href="https://smartstore.naver.com/aumu?nt_source=damoang&nt_medium=banner&nt_detail=garo"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_MJ2tjoQr_644bd3018095322f344c700c8367820d1b18b941.gif"></a><br/><br/>
-    <a href="https://smartstore.naver.com/farmartholic/products/10322631983"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_dHIuWmft_44800559d7ec84eb2084a7db531b586ef983f5d9.webp"></a><br/><br/>
-    <a href="https://smartstore.naver.com/skcwindowfilm/products/438041022"><img src="https://damoang.net/data/file/banners/71b4a9186c9a565849840477974bc730_vjMGQxhl_01e7a118ac11b5222a01471e70dbe10b770d8fa0.gif"></a><br/><br/>
+    <a alt="오일팡" href="https://smartstore.naver.com/oilpangone/products/9013411649"><img src="https://damoang.net/data/editor/2405/comment_237388223_aUnJ5cSX_fc8019b56b15177a0aadee58cd98def4a76a91e8.jpg" ></a><br/><br/>
+    <a alt="강릉하얀감자탕" href="https://smartstore.naver.com/souplab033/products/7792638781"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_fy36sYPM_4a140bd3f05852b90c5cfe639af9bdc7740f1443.webp" ></a><br/><br/>
+    <a alt="우리정보통신" href="https://mj123123.cafe24.com/intking"><img src="https://damoang.net/data/editor/2405/comment_3716871170_F28Rqvax_b69648f787231953208fb7d3573a7d34e73e2cee.gif" ></a><br/><br/>
+    <a alt="본메레" href="https://smartstore.naver.com/bonnemere/products/10384521569"><img src="https://damoang.net/data/editor/2405/comment_1954376459_N5F7JYMy_825e0f9fc63e9a275c36ff89784b82356651c75e.gif" ></a><br/><br/>
+    <a alt="제이앤오코퍼레이션" href="https://buddyback.co.kr/category/%EC%8A%A4%ED%8A%B8%EB%A0%88%EC%B9%AD%EA%B8%B0%EC%96%B4/24/"><img src="hhttps://damoang.net/data/editor/2405/comment_1954376459_8Pp2Jkcx_b61a73f34a8e465c167869757ca2ff23e79aa0e6.gif" ></a><br/><br/>
+    <a alt="바닷" href="https://1chopick.com/product/list.html?cate_no=61"><img src="https://damoang.net/data/editor/2405/comment_1954376459_XOulJKak_0fe54e522f29f391b1246ef49f4bbfd5e70550ba.gif" ></a><br/><br/>
+    <a alt="제이크레아컴퍼니" href="https://smartstore.naver.com/aumu?nt_source=damoang&nt_medium=banner&nt_detail=garo"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_dHIuWmft_44800559d7ec84eb2084a7db531b586ef983f5d9.webp" ></a><br/><br/>
+    <a alt="베리모먼트" href="hhttps://smartstore.naver.com/farmartholic/products/10322631983"><img src="https://damoang.net/data/editor/2405/comment_237388223_aUnJ5cSX_fc8019b56b15177a0aadee58cd98def4a76a91e8.jpg" ></a><br/><br/>
+    <a alt="올필름" href="https://smartstore.naver.com/skcwindowfilm/products/438041022"><img src="https://damoang.net/data/file/banners/71b4a9186c9a565849840477974bc730_vjMGQxhl_01e7a118ac11b5222a01471e70dbe10b770d8fa0.gif" ></a><br/><br/>
+    <a alt="알서포트" href="https://content.rview.com/ko/landing-damoang/?utm_source=damoang&utm_medium=cpc&utm_campaign=rvko-lnd-damoang"><img src="https://damoang.net/data/file/banners/e0c519ad5bcd4d56e72a26556e8d9765_CpVIksYG_09dd2ad3c72c37f55a7939d9b3890a0d6d65598b.gif" ></a><br/><br/>
+    <a alt="소금 한 톨" href="https://smartstore.naver.com/incasalt/products/8749778288"><img src="https://damoang.net/data/file/banners/3caac7cdc7671dc247c1198f5a901c2c_zXeCDNI3_4fbab496b0bb34e0efdc43c75bbe424cf842f9bc.gif" ></a><br/><br/>
+    <a alt="바이센트" href="https://m.smartstore.naver.com/byscent_/products/8797786630"><img src="https://damoang.net/data/file/banners/e0c519ad5bcd4d56e72a26556e8d9765_ObgWrGhA_d4954634c354427885187ae456222982b8ff3ec1.gif" ></a><br/><br/>
+    <a alt="적운창" href="https://series.naver.com/novel/detail.series?productNo=10198780"><img src="https://damoang.net/data/file/banners/e0c519ad5bcd4d56e72a26556e8d9765_hP2mYpg3_718cebcd0b2687fd3c99df8676b9f8037b988b6c.webp" ></a><br/><br/>
+    <a alt="플래이버플랜" href="https://smartstore.naver.com/flavorplan/products/7624104583"><img src="https://damoang.net/data/file/banners/a270a400dee098ccf1d9be8bb45768a0_lt04w9q6_dd9410b32daf4f965c926d77b54c257b9910fd93.gif" ></a><br/><br/>
+    <a alt="일딩오" href="https://smartstore.naver.com/ildio"><img src="https://damoang.net/data/file/banners/5e66ab3f5657fa5d5b92b43e91145d8a_E9Aam2I3_65702721937a30691bf9d8d43bb1f9ac18889b42.gif" ></a><br/><br/>
 
     <h3>사이드 배너</h3>
     <a href="https://mj123123.cafe24.com/intking"><img src="https://damoang.net/data/file/main/015f0b5e4ca48f7d44e78ebd6f62e722_okJwxveB_c6b9ed39b9396558318ccdda410aed0e2a1cfdbd.png"></a>
     <a href="https://smartstore.naver.com/lostone_seoul"><img src="https://damoang.net/data/editor/2405/comment_3716871170_0XBn5AoL_335c5bc4344705c5f3986db1a223e275b03e9198.gif"></a><br/><br/>
     <a href="https://www.griun.co.kr/?utm_source=damoang&utm_medium=banner"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_KaHgzveh_4dbfb09b1902d5fb477d8e8e988dc9d0ce639d57.webp">
     <a href="https://smartstore.naver.com/skcwindowfilm/products/438041022"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_uLbzS9tr_ab700a98eaf5bff85b60ce14379e5960bf463b09.webp"></a><br/><br/>
-    <a href="https://bit.ly/3ymGlEj "><img src="https://damoang.net/data/file/banners/f28b5896bb5b70ec2e61f0a18713be5a_BehOx5U6_87f0076c5bba3fc00b8d6db96d24ea8bf836ae65.jpg"></a><br/><br/>
-    6번 째 사이드바 광고주님 자리 <br/><br/>
-    7번 째 사이드바 광고주님 자리 <br/><br/>
-    8번 째 사이드바 광고주님 자리 <br/><br/>
+    <a href="https://bit.ly/3ymGlEj"><img src="https://damoang.net/data/file/banners/f28b5896bb5b70ec2e61f0a18713be5a_BehOx5U6_87f0076c5bba3fc00b8d6db96d24ea8bf836ae65.jpg"></a><br/><br/>
+    <a href="https://smartstore.naver.com/steady_coffee/products/10174775687"><img src="https://damoang.net/data/file/banners/3caac7cdc7671dc247c1198f5a901c2c_bhOsJ9yj_b07921c2ee45cc8619c0649f940dec64c3bfea4a.jpg"></a><br/><br/>
+    <a href="https://smartstore.naver.com/atelierhaus/products/10382323512"><img src="https://damoang.net/data/file/banners/e0c519ad5bcd4d56e72a26556e8d9765_CJpub748_8e60b790bef3076042bd66c1807d8fc3340dfada.gif"></a><br/><br/>
+    <a href="https://smartstore.naver.com/k-young/products/4605987515"><img src="https://damoang.net/data/file/banners/3caac7cdc7671dc247c1198f5a901c2c_WhdUsIZi_d7df65cb104838cf08fbf90ba21e40e66b370103.gif"></a><br/><br/>
     9번 째 사이드바 광고주님 자리 <br/><br/>
     10번 째 사이드바 광고주님 자리 <br/><br/>
 
@@ -46,5 +51,14 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     씽크와이드 <br/>
     지피컴퍼니 <br/>
     (주)알로식품연구소 <br/>
+    NHN커머스<br/>
+    빠누 <br/>
+    모찌덕후 <br/>
+    비트리비 서비스<br/>
+    LG헬로비전<br/><br/>
+
+
+    <h3>다모앙에 도움을 주셨던 이전 광고주님</h3>
+    <a href="https://smartstore.naver.com/dingreen"><img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_eIEiASdk_ae77791f0b971a127e0c4ea6f9bbba4692a8de02.gif"></a><br/><br/>
 
 </div>
