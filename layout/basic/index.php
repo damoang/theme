7fd824c860d491711dd7c6ffc3e56350de6e1898
김선도 <98006861+damaong@users.noreply.github.com>
2024-06-05 17:01:18 +0900

    Update index.php

--- layout/basic/index.php
+++ layout/basic/index.php
@@ -1,5 +1,9 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+// 현재 날짜와 시간을 'Y-m-d H:i:s' 형식으로 지정
+$dt = new DateTime();
+$formatted_dt = $dt->format('Y-m-d H:i:s');
+    
 ?>
 
 <div id="main-wrap" class="bg-body">
@@ -12,7 +16,8 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
                         <div class="col">
                             <!-- 위젯 시작 { -->
                             <h3 class="fs-5 px-3 py-2 mb-0">
-                                <a href="">
+                                    <a href="/free?bo_table=free&sop=and&sod=desc&sfl=wr_datetime&stx=<?php echo urlencode($formatted_dt); ?>&sca=&page=1&sst=wr_good">
+
                                     <i class="bi bi-chat-dots"></i>
                                     추천 글 (오늘 기준)
                                     <i class="bi bi-plus small float-end mt-1 text-body-tertiary"></i>
@@ -194,6 +199,18 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
                     </div>
 
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6922133409882969"
+                         crossorigin="anonymous"></script>
+                    <!-- 수평형 -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-6922133409882969"
+                         data-ad-slot="5448923097"
+                         data-ad-format="auto"
+                         data-full-width-responsive="true"></ins>
+                    <script>
+                         (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
 
                     <!-- 위젯 시작 { -->
                     <h3 class="fs-5 px-3 py-2 mb-0">
