75081eb702ecbacf6bfb9a37ccf19946ab3a5bb6
김선도 <98006861+damaong@users.noreply.github.com>
2024-06-06 01:40:57 +0900

    Update advertiser.php

--- page/advertiser.php
+++ page/advertiser.php
@@ -83,6 +83,11 @@ if (!defined('_GNUBOARD_')) {
                 <img src="https://damoang.net/data/file/banners/5e66ab3f5657fa5d5b92b43e91145d8a_E9Aam2I3_65702721937a30691bf9d8d43bb1f9ac18889b42.gif" alt="일딩오" class="mw-100">
             </a>
         </li>
+        <li class="list-group-item border-0">
+          <a href="https://smartstore.naver.com/dingreen" data-dd-action-name="Boards Ads Banner">
+               <img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_eIEiASdk_ae77791f0b971a127e0c4ea6f9bbba4692a8de02.gif" alt="딩그린" class="mw-100">
+            </a>
+        </li>
     </ul>
 </section>
 
@@ -187,11 +192,7 @@ if (!defined('_GNUBOARD_')) {
     </h3>
         <ul class="text-center row row-cols-2 row-cols-md-3 list-group list-group-horizontal list-group-flush" style="font-size: 0.875rem;">
 
-<!--             <a href="https://smartstore.naver.com/dingreen" data-dd-action-name="Boards Ads Banner">
-                딩그린 (베트남)  -->
-<!--                 <img src="https://damoang.net/data/file/banners/015f0b5e4ca48f7d44e78ebd6f62e722_eIEiASdk_ae77791f0b971a127e0c4ea6f9bbba4692a8de02.gif" alt="딩그린" class="mw-100">
-            </a> -->
-                    <li class="col list-group-item border-0">딩그린(베트남) 긴배너</li>
+<!--                     <li class="col list-group-item border-0">딩그린(베트남) 긴배너</li> -->
 
         </li>
              </ul>
